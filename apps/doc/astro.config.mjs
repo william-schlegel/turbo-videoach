27dd935926f3f8b909e9021a02c3349fa1ab52c8
william-schlegel <william.schlegel@gmail.com>
2023-02-16 11:25:50 +0100

    add astro doc workspace

---
+++ apps/doc/astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://videoach.com',
+	integrations: [mdx(), sitemap()],
+	server: {
+		port: 8080,
+	}
+});
